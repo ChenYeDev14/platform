0a9f30cbb777becf49aaca96815d957bc745663c
Chen <563301268@qq.com>
2012-07-02 06:34:35 +0800

    add ConnectionRefference

--- NormalBattle.h
+++ NormalBattle.h
@@ -12,21 +12,25 @@ private:
     QString map_location;//地图地址
     bool is_stoped;
     int human; //人机对战时值代表人的一方，若是双Ai对战，值为-1
+    int debug; //调试模式时代表需调试Ai一方，否则为-1
 
 //    Replay replay;
     void StartTwoAiBattle();
     void StartHumanAiBattle();
+    void StartDebugBattle();
     void Battle();      //进行对战
 
 protected:
     void run();
+
+  //public接口：
 
 public:
 
     QString GetReplayPath();
 
 public slots:
-    void start_normal_battle(QString side1, QString side2, QString map);//开始对战
+    void start_normal_battle(QString side1, QString side2, QString map);//开始对战，side1,side2为Ai路径，map为地图路径
     void connect();
     void stop();            //提供终止线程的接口
 
@@ -36,7 +40,7 @@ signals:
     void send_winner(int);   //判断胜利方, winner = 0 代表1号选手获胜，winner =1 代表2号选手获胜, winner = 2代表战斗平局
                             //判断胜利方，3代表1号超时2号获胜，4代表2号超时1号获胜，5代表双方超时的平局。
     void round(int);//返回当前运行到第几回合
-    void ready_for_connect(QString name); //人机对战时返回管道名以便与平台连接
+    void ready_for_connect(QString name); //人机对战时返回服务名以便与平台连接
 };
 
 
